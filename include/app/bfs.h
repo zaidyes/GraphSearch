866806deea25a0c085403347c25201310a6dfd94
Muhammed Zaid Alam <zaidyes@gmail.com>
2018-02-15 04:27:30 -0800

    Added: BFS using brute force approach no cyclic check

--- include/app/bfs.h
+++ include/app/bfs.h
@@ -32,8 +32,52 @@ class BreadthFirstSearch {
          * 
          */
         NodeRef find(GraphRef graph, const std::string& id) {
+            if (!graph) {
+                Log::error("Graph invalid...skipping search");
+                return nullptr;
+            }
 
-			// @TODO: Implement
+            std::cout << "id:" << id << std::endl;
+
+            // The vector holds our visited nodes
+            //std::vector<NodeRef> visitedNodes;
+            //visitedNodes.reserve(graph->size());
+
+            std::queue<NodeRef> toProcess;
+
+            auto rootNode = graph->getFirst();
+            //visitedNodes.push_back(rootNode);
+            toProcess.push(rootNode);
+
+            while(!toProcess.empty()) {
+
+                auto node = toProcess.front();
+                toProcess.pop();
+                //Log::infof("now processing id: %d", node->getId());
+
+                if (node->getId() == id) {
+                    std::cout << "found id:" << id << std::endl;
+                    return node;
+                }
+
+                auto connections = node->getConnections();
+                for(auto itr = connections.begin(); itr != connections.end(); ++itr) {
+                    if (auto lockedNode = (*itr).lock()) {
+                        /*auto visitedItr = std::find(visitedNodes.begin(), visitedNodes.end(), lockedNode);
+                        // skip the node if its already visited else queue it up
+                        if (visitedItr == visitedNodes.end()) {
+                            visitedNodes.push_back(lockedNode);
+                            toProcess.push(lockedNode);
+                        }*/
+                        toProcess.push(lockedNode);
+                    }
+                }
+
+            }
+
+            std::cout << "not found id:" << id << std::endl;
+
+
 			return nullptr;
 
         }
